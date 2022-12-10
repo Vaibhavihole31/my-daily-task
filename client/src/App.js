9352459a7ca191e39752495b44bcd9cbba2982ad
Vaibhavihole31 <itspinki05@gmail.com>
2022-12-10 23:50:00 +0530

    feat: home and addtask view add

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="container">
-      {/* Remove this code and place your own code here. */}
-      <div class="d-flex align-items-center justify-content-center" style={{height: '80vh'}}>
-        <h1>MERN Starter is Up and Running 🚀</h1>
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
