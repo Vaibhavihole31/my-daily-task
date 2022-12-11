e3bbac67c7033dce701f4bc1682874722cd7e85c
Vaibhavihole31 <itspinki05@gmail.com>
2022-12-11 11:21:11 +0530

    feat: home page

--- client/src/components/TaskCard/TaskCard.js
+++ client/src/components/TaskCard/TaskCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import "./TaskCard.css"
 
-function TaskCard() {
+function TaskCard({title, content}) {
   return (
-   <>
-   
-   </>
+    <div className='card-note'>
+      <h6 className='card-note-title'>{title}</h6>
+      <p className='card-note-content'>{content}</p>
+    </div>
   )
 }
 
--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import TaskCard from '../../components/TaskCard/TaskCard';
+import axios from 'axios';
+import './Home.css'
 
 function Home() {
+
+  const [tasks, setTasks] =  useState("");
+  
+  useEffect(() => {
+    axios.get('/task').then(res => {
+      setTasks(res.data);
+      console.log(res.data);
+    })
+  },[])
+
   return (
-    <div>Home</div>
+    <>
+    <div className='contanier'>
+    <div className='app-title-container'>
+        <h1 className='app-title'>📝 Daily Task</h1>
+      </div>
+      <div className='row'>
+      <div className='notes-container '>
+        {
+          tasks && tasks.map((task, i) => {
+            return (
+              <>
+              <TaskCard 
+              key = {i}
+              title = {task.title}
+              content={task.content}
+              />
+              </>
+            )
+          })
+        }
+      </div>
+      </div>
+    </div>
+    </>
   )
 }
 
